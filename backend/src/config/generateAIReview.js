2f66ce5a48f1fe104aa76ebd801ac4b3c94e6006
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-10-11 02:04:51 +0530

    "Deleted gemini.js file containing code for generating AI reviews for pull requests"

--- backend/src/config/generateAIReview.js
+++ backend/src/config/generateAIReview.js
@@ -1,3 +1,5 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 const generateAIReview = async (title,body,diff) => {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
